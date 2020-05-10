e15d78f5f79e091cd8542dcaa677fdbf30ec6d89
Ali Abdulmadzhidov <mrfreezzzer@mail.ru>
2020-05-10 15:36:01 +0300

    development
    Signed-off-by: Ali Abdulmadzhidov <mrfreezzzer@mail.ru>

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 let letters = ["a", "б", "в", "г", "г1", "гь", "гъ", "д", "дж", "е", "ж", "з", "и", "к", "к1", "к11", "къ", "кь", "кь1", "л", "л1", "лъ", "м", "н", "о", "п", "п1", "р", "с", "т", "т1", "у", "у", "ф", "х", "х1", "хъ", "хь", "ц", "ц1", "ц11", "ч", "ч1", "ч11", "ш", "щ", "э", "ю", "я"];
 
-const getLetterHtml = (letter) => `<div class='letter'>${letter.toUpperCase()}</div>`;
+let symbols = ["a"]
+
+const getLetterHtml = (index) => `<div class='letter'><img class="image" src="img/${symbols[0]}.png" /></br><h1 class="text">${letters[index].toUpperCase()}</h1></div>`;
 
 jQuery(document).ready(function(){
 for (var i = 0; i < letters.length; i++) {
-	$(".wrapper").append(getLetterHtml(letters[i]))
+	$(".wrapper").append(getLetterHtml(i))
 }
 })
