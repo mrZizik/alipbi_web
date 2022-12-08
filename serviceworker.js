9885e33cada9cac75ab7a4f04aacc2f8fa93bb5e
mrzizik <mrfreezzzer@mail.ru>
2022-12-08 23:46:26 +0300

    service worket log

--- serviceworker.js
+++ serviceworker.js
@@ -12,6 +12,7 @@ const CACHE_NAME = "static-cache-v2";
 self.addEventListener("install", (evt) => {
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      console.log("Service worker installed");
       return cache.addAll(FILES_TO_CACHE);
     }),
   );
